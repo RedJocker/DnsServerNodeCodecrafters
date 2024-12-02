0c81e4ff0a8143557bf240b855cf8210bf974f54
redjocker <mauricio.guaruja@gmail.com>
2024-12-02 01:44:21 -0300

    Header

---
+++ app/header.js
@@ -0,0 +1,194 @@
+class Header {
+    /**
+    * Creates a Header instance.
+    * @param {number} packetIdentifier - A unique identifier for the packet.
+    * @param {number} queryOrResponseIndicator - Indicates if it's a query (1) or a response (0).
+    * @param {number} operationCode - Specifies the operation code.
+    * @param {number} authoritativeAnswer - Indicates if the server is authoritative (1) or not (0).
+    * @param {number} truncation - Indicates if the message is truncated (1) or not (0).
+    * @param {number} recursionDesired - Specifies if recursion is desired (1) or not (0).
+    * @param {number} recursionAvailable - Specifies if recursion is available (1) or not (0).
+    * @param {number} reserved - Reserved for future use.
+    * @param {number} responseCode - Indicates the response code.
+    * @param {number} questionCount - The number of questions in the message.
+    * @param {number} answerRecordCount - The number of answers in the message.
+    * @param {number} authorityRecordCount - The number of authority records in the message.
+    * @param {number} additionalRecordCount - The number of additional records in the message.
+    */
+    constructor(
+      packetIdentifier,
+      queryOrResponseIndicator,
+      operationCode,
+      authoritativeAnswer,
+      truncation,
+      recursionDesired,
+      recursionAvailable,
+      reserved,
+      responseCode,
+      questionCount,
+      answerRecordCount,
+      authorityRecordCount,
+      additionalRecordCount
+    ) {
+      this.packetIdentifier = packetIdentifier;
+      this.queryOrResponseIndicator = queryOrResponseIndicator;
+      this.operationCode = operationCode;
+      this.authoritativeAnswer = authoritativeAnswer;
+      this.truncation = truncation;
+      this.recursionDesired = recursionDesired;
+      this.recursionAvailable = recursionAvailable;
+      this.reserved = reserved;
+      this.responseCode = responseCode;
+      this.questionCount = questionCount;
+      this.answerRecordCount = answerRecordCount;
+      this.authorityRecordCount = authorityRecordCount;
+      this.additionalRecordCount = additionalRecordCount;
+    }
+  
+    // Method to represent the object as a string
+    toString() {
+      return JSON.stringify(this, null, 2);
+    }
+  
+    // Static method to create a Header from a plain object
+    static fromObject(obj) {
+      return new Header(
+        obj.packetIdentifier,
+        obj.queryOrResponseIndicator,
+        obj.operationCode,
+        obj.authoritativeAnswer,
+        obj.truncation,
+        obj.recursionDesired,
+        obj.recursionAvailable,
+        obj.reserved,
+        obj.responseCode,
+        obj.questionCount,
+        obj.answerRecordCount,
+        obj.authorityRecordCount,
+        obj.additionalRecordCount
+      );
+    }
+
+    toBuffer() {
+        const buffer = Buffer.alloc(12)
+        buffer.writeUInt16BE(this.packetIdentifier)
+        const thirdByte = (this.queryOrResponseIndicator << 7) 
+            + (this.operationCode << 3)
+            + (this.authoritativeAnswer << 2)
+            + (this.truncation << 1)
+            + (this.recursionDesired)
+        buffer.writeUInt8(thirdByte)
+        const fourthByte = (this.recursionAvailable << 7) 
+            + (this.reserved << 4)
+            + (this.responseCode)
+        buffer.writeUInt8(fourthByte)
+        buffer.writeUInt16BE(this.questionCount)
+        buffer.writeUInt16BE(this.answerRecordCount)
+        buffer.writeUInt16BE(this.authorityRecordCount)
+        buffer.writeUInt16BE(this.additionalRecordCount)
+        return buffer;
+    }
+}
+
+class HeaderBuilder {
+
+    constructor() {
+        this.packetIdentifier = 1024;
+        this.queryOrResponseIndicator = 1;
+        this.operationCode = 0;
+        this.authoritativeAnswer = 0;
+        this.truncation = 0;
+        this.recursionDesired = 0;
+        this.recursionAvailable = 0;
+        this.reserved = 0;
+        this.responseCode = 0;
+        this.questionCount = 0;
+        this.answerRecordCount = 0;
+        this.authorityRecordCount = 0;
+        this.additionalRecordCount = 0;
+  }
+
+  setPacketIdentifier(value) {
+    this.packetIdentifier = value;
+    return this;
+  }
+
+  setQueryOrResponseIndicator(value) {
+    this.queryOrResponseIndicator = value;
+    return this;
+  }
+
+  setOperationCode(value) {
+    this.operationCode = value;
+    return this;
+  }
+
+  setAuthoritativeAnswer(value) {
+    this.authoritativeAnswer = value;
+    return this;
+  }
+
+  setTruncation(value) {
+    this.truncation = value;
+    return this;
+  }
+
+  setRecursionDesired(value) {
+    this.recursionDesired = value;
+    return this;
+  }
+
+  setRecursionAvailable(value) {
+    this.recursionAvailable = value;
+    return this;
+  }
+
+  setReserved(value) {
+    this.reserved = value;
+    return this;
+  }
+
+  setResponseCode(value) {
+    this.responseCode = value;
+    return this;
+  }
+
+  setQuestionCount(value) {
+    this.questionCount = value;
+    return this;
+  }
+
+  setAnswerRecordCount(value) {
+    this.answerRecordCount = value;
+    return this;
+  }
+
+  setAuthorityRecordCount(value) {
+    this.authorityRecordCount = value;
+    return this;
+  }
+
+  setAdditionalRecordCount(value) {
+    this.additionalRecordCount = value;
+    return this;
+  }
+
+  build() {
+    return new Header(
+      this.packetIdentifier,
+      this.queryOrResponseIndicator,
+      this.operationCode,
+      this.authoritativeAnswer,
+      this.truncation,
+      this.recursionDesired,
+      this.reserved,
+      this.responseCode,
+      this.questionCount,
+      this.answerRecordCount,
+      this.authorityRecordCount,
+      this.additionalRecordCount
+    );
+  }
+}
+
+export { Header, HeaderBuilder };
--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 import dgram from "dgram"
+import { HeaderBuilder } from "./header.js";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -10,7 +11,11 @@ udpSocket.bind(2053, "127.0.0.1");
 udpSocket.on("message", (buf, rinfo) => {
   try {
     const response = Buffer.from("ok");
-    udpSocket.send(response, rinfo.port, rinfo.address);
+    
+    const defaultHeader = new HeaderBuilder().build()
+    const headerBuffer = defaultHeader.toBuffer()
+    
+    udpSocket.send(headerBuffer, rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
   }
@@ -24,3 +29,38 @@ udpSocket.on("listening", () => {
   const address = udpSocket.address();
   console.log(`Server listening ${address.address}:${address.port}`);
 });
+
+
+
+
+
+
+/*
+Field 	Size 	Description
+Packet Identifier (ID) 	16 bits 	A random ID assigned to query packets. Response packets must reply with the same ID.
+Expected value: 1234.
+Query/Response Indicator (QR) 	1 bit 	1 for a reply packet, 0 for a question packet.
+Expected value: 1.
+Operation Code (OPCODE) 	4 bits 	Specifies the kind of query in a message.
+Expected value: 0.
+Authoritative Answer (AA) 	1 bit 	1 if the responding server "owns" the domain queried, i.e., it's authoritative.
+Expected value: 0.
+Truncation (TC) 	1 bit 	1 if the message is larger than 512 bytes. Always 0 in UDP responses.
+Expected value: 0.
+Recursion Desired (RD) 	1 bit 	Sender sets this to 1 if the server should recursively resolve this query, 0 otherwise.
+Expected value: 0.
+Recursion Available (RA) 	1 bit 	Server sets this to 1 to indicate that recursion is available.
+Expected value: 0.
+Reserved (Z) 	3 bits 	Used by DNSSEC queries. At inception, it was reserved for future use.
+Expected value: 0.
+Response Code (RCODE) 	4 bits 	Response code indicating the status of the response.
+Expected value: 0 (no error).
+Question Count (QDCOUNT) 	16 bits 	Number of questions in the Question section.
+Expected value: 0.
+Answer Record Count (ANCOUNT) 	16 bits 	Number of records in the Answer section.
+Expected value: 0.
+Authority Record Count (NSCOUNT) 	16 bits 	Number of records in the Authority section.
+Expected value: 0.
+Additional Record Count (ARCOUNT) 	16 bits 	Number of records in the Additional section.
+Expected value: 0.
+*/
